901c059e62dde56745f83b303312b442fa5276e4
dot_e <eul.nelson@gmail.com>
2019-02-27 15:31:08 +0100

    save erroor to Heroku Logs

--- config/app.php
+++ config/app.php
@@ -15,6 +15,9 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    //Log
+    'log’=>’errorlog',
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
